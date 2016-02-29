0e6a6f8959f5c8d474ba8e76f13cb9619a9b2295
SelAdv-G1-Member-1 <s.eleniumadvancelg@gmail.com>
2016-02-29 16:49:45 +0700

    Commit to Member1 branch

--- DemoProject1/ThanhLe.cs
+++ DemoProject1/ThanhLe.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DemoProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class ThanhLe
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Console.WriteLine("Hello");
         }
     }
 }
