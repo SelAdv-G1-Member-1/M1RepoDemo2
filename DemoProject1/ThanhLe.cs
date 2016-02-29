0ef7db8e2b0604b3c260b8d75dabe46b114090ce
SelAdv-G1-Member-2 <se.leniumadvancelg@gmail.com>
2016-02-29 17:27:07 +0700

    Edit TC

--- DemoProject1/ThanhLe.cs
+++ DemoProject1/ThanhLe.cs
@@ -7,9 +7,15 @@ namespace DemoProject1
     public class ThanhLe
     {
         [TestMethod]
+        public void TestMethodaaa()
+        {
+            Console.WriteLine("Hello Thanh 2");
+        }
+
+        [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Thanh 3");
         }
     }
 }
