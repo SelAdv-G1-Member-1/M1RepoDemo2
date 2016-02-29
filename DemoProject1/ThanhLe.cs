4df2561d1435d059ee4946e3e17ad45aedc4b283
SelAdv-G1-Member-1 <s.eleniumadvancelg@gmail.com>
2016-02-29 17:38:04 +0700

    a

--- DemoProject1/ThanhLe.cs
+++ DemoProject1/ThanhLe.cs
@@ -7,6 +7,12 @@ namespace DemoProject1
     public class ThanhLe
     {
         [TestMethod]
+        public void TestMethodaaa()
+        {
+            Console.WriteLine("Hello Thanh 2");
+        }
+
+        [TestMethod]
         public void TestMethod1()
         {
             Console.WriteLine("Hello Thanh 1");
